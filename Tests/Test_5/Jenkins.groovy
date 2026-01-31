4b42ba00a714dd2fd5e49f34742f7b9727208bea
Tawan Duangchit <murasameenma@gmail.com>
2026-02-01 02:41:25 +0700

    jenkins

--- Tests/Test_5/Jenkins.groovy
+++ Tests/Test_5/Jenkins.groovy
@@ -1,15 +1,34 @@
 pipeline {
-    agent {-----}
+    agent any
     stages {
         stage('Checkout Code From Git') {
+            steps {
+                checkout scm
+            }
         }
     }
     stages {
         stage('Run Test Automate') {
+            steps {
+                bat 'python -m robot -d results Tests/Test_2'
+            }
         }
     }
     stages {
         stage('Send Result To Jenkins') {
+            script {
+                step([$class: 'RobotPublisher',
+                    outputPath: 'results',
+                    outputFileName: 'output.xml',
+                    reportFileName: 'report.html',
+                    logFileName: 'log.html'
+                ])
+            }
+        }
+    }
+    post {
+        always {
+            archiveArtifacts artifacts: 'results/*.html', allowEmptyArchive: true
         }
     }
 }
